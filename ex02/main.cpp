d78626b050ce6988c63ada25ab422814942a8be3
lguiet <lguiet@2D2.42angouleme.fr>
2025-05-21 15:06:28 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguiet <lguiet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 14:41:38 by lguiet            #+#    #+#             */
+/*   Updated: 2025/05/21 15:04:23 by lguiet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main (void)
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+
+    std::cout<<"string adress: "<<&string<<std::endl;
+    std::cout<<"ptdr points to adress: "<<stringPTR<<std::endl;
+    std::cout<<"ref adress: "<<&stringREF<<std::endl;
+
+    std::cout<<"string content: "<<string<<std::endl;
+    std::cout<<"ptdr points to value: "<<*stringPTR<<std::endl;
+    std::cout<<"ref to string: "<<stringREF<<std::endl;
+
+    return (0);
+}
